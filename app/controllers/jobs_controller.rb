20119bdadbb283b26204913fc91d4fccff1b92d6
Kemal C <ddeveloperr@gmail.com>
2016-03-07 23:03:44 +0000

    Step 9 - Update Job Listings!

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,6 +14,15 @@ class JobsController < ApplicationController
    redirect_to jobs_path
    end
     
+   def edit
+       @job = Job.find(params[:id])
+   end
+   
+    def update
+      @job = Job.find(params[:id])
+      @job.update_attributes(job_params)
+      redirect_to jobs_path
+    end
     
     
     private

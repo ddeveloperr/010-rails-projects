73f296bf7e6f36bba1754d428d172ed23a53da20
Kemal C <ddeveloperr@gmail.com>
2016-03-08 09:42:27 +0000

    Step 10 - fix bugs in controller; Done.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,35 +1,40 @@
 class JobsController < ApplicationController
-    def index
-        @jobs = Job.all
-    end
-    
-    def new
-        @job = Job.new
-    end
-   
-   def create
-   p "In the create method!!!!!!"
-   job = Job.create(job_params)
-   p job
-   redirect_to jobs_path
-   end
-    
-   def edit
-       @job = Job.find(params[:id])
-   end
-   
-    def update
-      @job = Job.find(params[:id])
-      @job.update_attributes(job_params)
-      redirect_to jobs_path
-    end
-    
-    
-    private
 
-    def job_params
-      params.require(:job).permit(:title, :description)
-    end
-    
-    
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.delete
+    redirect_to jobs_path
+  end
+
 end
